30d2b69571f726744a683d0e69c53d390fa92788
Adithya J <adithya.j@adithya321.me>
2017-03-01 00:57:55 +0530

    :sparkles: Switch to Immersive Mode in Home

--- app/src/main/java/com/pimp/instincts/activities/HomeActivity.java
+++ app/src/main/java/com/pimp/instincts/activities/HomeActivity.java
@@ -26,6 +26,7 @@ import android.graphics.drawable.shapes.OvalShape;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -95,6 +96,21 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
         wheelView.setAdapter(new WheelAdapter(entries));
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        View decorView = getWindow().getDecorView();
+        if (hasFocus) {
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 
     private ArrayList<String> getImageList() {
