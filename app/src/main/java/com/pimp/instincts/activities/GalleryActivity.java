1b64acc92e368959bafce9c9834f6d286ef15655
Adithya J <adithya.j@adithya321.me>
2017-03-04 19:06:31 +0530

    :sparkles: Add Toolbar in Gallery

--- app/src/main/java/com/pimp/instincts/activities/GalleryActivity.java
+++ app/src/main/java/com/pimp/instincts/activities/GalleryActivity.java
@@ -22,6 +22,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.pimp.instincts.R;
 import com.pimp.instincts.adapters.GalleryAdapter;
@@ -35,12 +38,28 @@ public class GalleryActivity extends AppCompatActivity {
 
     @BindView(R.id.gallery_recycler_view)
     RecyclerView galleryRecyclerView;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        int mUIFlag = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+
+        getWindow().getDecorView().setSystemUiVisibility(mUIFlag);
+
         setContentView(R.layout.activity_gallery);
         ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         ArrayList<String> imagesList = new ArrayList<>();
         for (int i = 1; i <= 23; i++)
@@ -53,5 +72,30 @@ public class GalleryActivity extends AppCompatActivity {
         galleryRecyclerView.setHasFixedSize(true);
         galleryRecyclerView.setLayoutManager(gridLayoutManager);
         galleryRecyclerView.setAdapter(galleryAdapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        View decorView = getWindow().getDecorView();
+        if (hasFocus) {
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 }
